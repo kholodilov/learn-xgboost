f9c1502553bd106b0f0e1151219f5fb89deab272
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2017-02-12 03:13:02 +0300

    training with cross-validation and auc monitoring

---
+++ blocked.R
@@ -0,0 +1,23 @@
+require(e1071)
+require(xgboost)
+require(caret)
+
+read_libsvm_to_xgb <- function(file_path) {
+  libsvm <- read.matrix.csr(file = file_path, fac = FALSE)
+  xgb.DMatrix(as(libsvm$x, "dgCMatrix"), label = libsvm$y)
+}
+
+dtrain <- read_libsvm_to_xgb("blocked_train.libsvm")
+dtest <- read_libsvm_to_xgb("blocked_test.libsvm")
+
+nround <- 10
+param <- list(max_depth=3, eta=1, silent=1, nthread=8, objective='binary:logistic', eval_metric = "auc", eval_metric = "error")
+xgb_cv = xgb.cv(param, dtrain, nround, nfold=5)
+
+model <- xgb.train(data = dtrain, nrounds = nround, watchlis = list(train=dtrain, test=dtest), verbose = 1, eval_metric = "auc", eval_metric = "error")
+
+importance_matrix <- xgb.importance(model = model)
+print(importance_matrix)
+xgb.plot.importance(importance_matrix = importance_matrix)
+
+xgb.dump(model, with_stats = T)
